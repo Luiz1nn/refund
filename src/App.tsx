2085bea49e107cd257db1d8068ebedc34480672c
Luiz1nn <luisfernandobr2003@gmail.com>
2026-04-29 23:40:42 -0300

    ✨ feat: add react-query provider, browser router, and home page component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,17 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HomePage } from "~/pages/home.page";
+
+const queryClient = new QueryClient();
+
 export function App() {
-  return null;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<HomePage />} path="/" />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
 }
---
+++ src/pages/home.page.tsx
@@ -0,0 +1,7 @@
+export function HomePage() {
+  return (
+    <section className="p-10 bg-gray-500 flex flex-col gap-6 rounded-2xl max-w-270.5 mx-auto">
+      <h1 className="font-bold text-xl">Requests</h1>
+    </section>
+  );
+}
